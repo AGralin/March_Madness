a0db521ed1987105e11b24cb05803e8a17387e05
yenla9@gmail.com <yenla9@gmail.com>
2023-03-09 18:12:29 -0800

    add the fanshares

---
+++ FanShares.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.8.2/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.8.2/security/Pausable.sol";
+import "@openzeppelin/contracts@4.8.2/access/Ownable.sol";
+import "@openzeppelin/contracts@4.8.2/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts@4.8.2/token/ERC1155/extensions/ERC1155Supply.sol";
+
+contract Fanshares is ERC1155, Pausable, Ownable, ERC1155Burnable, ERC1155Supply {
+    constructor() ERC1155("") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address account, uint256 id, uint256 amount)
+        public
+    {
+        _mint(account, id, amount, "");
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+}
