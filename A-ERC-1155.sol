2e468f3a729f1c980242be26f49f6f00af096efe
AGralin <grali007@umn.edu>
2023-03-13 12:49:29 -0500

    added metadata folder/ update A-ERC file

--- A-ERC-1155.sol
+++ A-ERC-1155.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: MIT
+/**
+ __       __                                __                                 
+|  \     /  \                              |  \                                
+| $$\   /  $$  ______    ______    _______ | $$____                            
+| $$$\ /  $$$ |      \  /      \  /       \| $$    \                           
+| $$$$\  $$$$  \$$$$$$\|  $$$$$$\|  $$$$$$$| $$$$$$$\                          
+| $$\$$ $$ $$ /      $$| $$   \$$| $$      | $$  | $$                          
+| $$ \$$$| $$|  $$$$$$$| $$      | $$_____ | $$  | $$                          
+| $$  \$ | $$ \$$    $$| $$       \$$     \| $$  | $$                          
+ \$$      \$$  \$$$$$$$ \$$        \$$$$$$$ \$$   \$$                                                                             
+ __       __                  __                                               
+|  \     /  \                |  \                                              
+| $$\   /  $$  ______    ____| $$ _______    ______    _______   _______       
+| $$$\ /  $$$ |      \  /      $$|       \  /      \  /       \ /       \      
+| $$$$\  $$$$  \$$$$$$\|  $$$$$$$| $$$$$$$\|  $$$$$$\|  $$$$$$$|  $$$$$$$      
+| $$\$$ $$ $$ /      $$| $$  | $$| $$  | $$| $$    $$ \$$    \  \$$    \       
+| $$ \$$$| $$|  $$$$$$$| $$__| $$| $$  | $$| $$$$$$$$ _\$$$$$$\ _\$$$$$$\      
+| $$  \$ | $$ \$$    $$ \$$    $$| $$  | $$ \$$     \|       $$|       $$      
+ \$$      \$$  \$$$$$$$  \$$$$$$$ \$$   \$$  \$$$$$$$ \$$$$$$$  \$$$$$$$
+ */
+
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
@@ -10,18 +31,15 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MarchMadness is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
     uint256 public publicPrice = 0.001 ether;
-    bool public publicMintOpen = false;
+    bool public publicMintOpen = true;
+    mapping(uint => string) public tokenURI;
     constructor() ERC1155("") {}
 
-    // edit function to open public mint
+    // edit function to open/close public mint
     function editMintWindows(
         bool _publicMintOpen
     ) external onlyOwner {
         publicMintOpen = _publicMintOpen;
-    }
-    // URI?
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
     }
     // pause function
     function pause() public onlyOwner {
@@ -31,7 +49,7 @@ contract MarchMadness is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Sup
     function unpause() public onlyOwner {
         _unpause();
     }
-    // Public mint function
+    // public mint function
     function mint(uint256 id, uint256 amount)
         public
         payable
@@ -42,18 +60,30 @@ contract MarchMadness is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Sup
         _mint(msg.sender, id, amount, "");
     }
 
-    // Withdraw function
+    // withdraw function
     function withdraw(address _addr) external onlyOwner {
         uint256 balance = address(this).balance;
         payable(_addr).transfer(balance);
     }
+    // set URI
+    function setURI(uint _id, string memory _uri) external onlyOwner {
+    tokenURI[_id] = _uri;
+    emit URI(_uri, _id);
+    }
+    // call URI
+    function uri(uint _id) public override view returns (string memory) {
+    return tokenURI[_id];
+    }
 
+    /*
+    // URI
     function uri(uint256 _id) public view virtual override returns (string memory) {
         require(exists(_id), "URI: nonexistent token");
 
         return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json"));
     }
- 
+    */
+
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         public
         onlyOwner
